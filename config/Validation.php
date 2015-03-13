37193faa527748d7e1134c5001c5630555bdc02e
Lev Bezborodov <lev.bezborodov@gmail.com>
2015-03-13 18:41:18 +0300

    Missing changes added.

--- config/Validation.php
+++ config/Validation.php
@@ -1,19 +1,18 @@
 <?php if(!defined('BASE_PATH')) include $_SERVER['DOCUMENT_ROOT'] . '/404.php';
 
-$Validation['Image'] = array(
+$Config['Image'] = array(
 	'Width' => '_int',
 	'Height' => '_int',
-	'URL' => '_text'
+	'URL' => '_emptyText'
 );
-$Validation['Token'] = array(
+$Config['Token'] = array(
 	'Content' => '_text',
 	'Type' => array('session','activate','restorePassword'),
 	'UserID' => '_id'
 );
-$Validation['User'] = array(
+$Config['User'] = array(
 	'Email' => '_text',
 	'Password' => '_text',
 	'FirstName' => '_text',
-	'LastName' => '_text'
+	'LastName' => '_emptyText'
 );
-$locales = array('ru','en');
